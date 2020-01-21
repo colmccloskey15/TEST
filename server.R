963e887e1a386afbf977723286e2cfafb13b514b
colmccloskey15 <60145373+colmccloskey15@users.noreply.github.com>
2020-01-21 10:25:14 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -9,13 +9,15 @@
 
 library(shiny)
 
+my_data <- read.csv("Data.csv", header=TRUE)
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     output$distPlot <- renderPlot({
 
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
+        x    <- my_data[, 2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
